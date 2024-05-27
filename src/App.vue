4cce6efb11d9714a6ef7761a4999e840d455c50b
GuglielmoConsolini <consoliniguglielmo@gmail.com>
2024-05-27 16:10:16 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import CardList from './components/CardList.vue';
+import HeaderApp from './components/HeaderApp.vue';
+export default {
+  components: {
+    CardList,
+    HeaderApp,
+  },
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    
+  },
+  mounted() {
+    
+  }
+}
+</script>
+
+<template>
+  <HeaderApp />
+  <CardList />
+</template>
+
+<style>
+
+</style>
---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,17 @@
+<script>
+  export default {
+    name: "HeaderApp"
+  }
+</script>
+
+<template>
+  <div class="container-fluid border border-warning">
+    <div class="col text-left">
+        <h1>Yu-gi-oh API</h1>
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
